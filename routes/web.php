506cf8f75bbc14b56e5fe6f2178f11805f89cf62
nahashon <nashonmwendwa0@gmail.com>
2025-06-03 10:34:57 +0300

    Set up Admin, Vendor, and Customer dashboard controllers, views, and routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\Vendor\DashboardController as VendorDashboard;
+use App\Http\Controllers\Customer\DashboardController as CustomerDashboard;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,6 +14,24 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+//Admin Routes
+Route::prefix('admin')->group(function (){
+    Route::get('dashboard', [AdminDashboard::class, 'index'])->name('admin.dashboard');
+});
+
+//Vendor Routes
+Route::prefix('vendor')->group(function(){
+    Route::get('dashboard', [VendorDashboard::class, 'index'])->name('vendor.dashboard');
+});
+
+//Customer Routes
+Route::prefix('customer')->group(function(){
+    Route::get('dashboard', [CustomerDashboard::class, 'index'])->name('customer.dashboard');
+});
+
+//Product Routes
+Route::resource('products', ProductController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
